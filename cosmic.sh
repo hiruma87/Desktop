b85aba2f1166b8c6d33c59833fe2b1a601a66ff4
hiruma87 <96128631+hiruma87@users.noreply.github.com>
2024-11-19 21:12:55 +0800

    Create cosmic.sh

---
+++ cosmic.sh
@@ -0,0 +1,145 @@
+#!/usr/bin/env bash
+#-------------------------------------------------------------------------
+#      _          _    __  __      _   _
+#     /_\  _ _ __| |_ |  \/  |__ _| |_(_)__
+#    / _ \| '_/ _| ' \| |\/| / _` |  _| / _|
+#   /_/ \_\_| \__|_||_|_|  |_\__,_|\__|_\__|
+#  Arch Linux Post Install Setup and Config
+#-------------------------------------------------------------------------
+
+echo
+echo "INSTALLING AUR SOFTWARE"
+echo
+
+cd "${HOME}"
+mkdir git
+cd git
+echo "CLONING: YAY"
+git clone "https://aur.archlinux.org/yay.git"
+cd yay
+makepkg -si --noconfirm
+
+func_install() {
+	if pacman -Qi $1 &> /dev/null; then
+		tput setaf 2
+  		echo "###############################################################################"
+  		echo "################## The package "$1" is already installed"
+      		echo "###############################################################################"
+      		echo
+		tput sgr0
+	else
+    	tput setaf 3
+    	echo "###############################################################################"
+    	echo "##################  Installing package "  $1
+    	echo "###############################################################################"
+    	echo
+    	tput sgr0
+    	yay -S --noconfirm --needed $1
+    fi
+}
+
+PKGS=(
+#basic cinnamon desktop
+xorg-server
+egl-wayland
+xorg-xinit
+xorg-xwininfo
+cosmic
+cosmic-comp
+cosmic-applets
+cosmic-app-library
+cosmic-bg
+cosmic-icon-theme
+cosmic-launcher
+cosmic-notifications
+cosmic-osd
+cosmic-panel
+cosmic-setting
+cosmic-text-editor
+cosmic-files
+cosmic-terminal
+cosmic-wallpapers
+ #utilities
+cups
+cups-pdf
+gutenprint #canon driver
+unzip
+unrar
+flatpak
+grub-btrfs
+btrfs-assistant
+python-pipx
+wireless_tools
+inotify-tools
+jq
+#bluetooth
+bluez
+bluez-utils
+bluez-libs
+#audio
+pipewire
+wireplumber
+pipewire-alsa
+pipewire-jack
+pipewire-pulse
+libpulse
+gst-plugin-pipewire
+pavucontrol
+# applications
+firefox
+opera
+thunderbird
+# fonts
+noto-fonts
+noto-fonts-cjk
+noto-fonts-emoji
+ttf-ubuntu-font-family
+ttf-roboto
+ttf-roboto-mono
+)
+
+for PKG in "${PKGS[@]}"; do
+	count=$[count+1]
+	echo "################################################################"
+	tput setaf 3;echo "Installing package nr.  "$count " " $PKG;tput sgr0;
+	echo "################################################################"
+	func_install $PKG
+    sleep 1
+done
+
+sudo systemctl enable cups
+sleep 1
+sudo systemctl enable bluetooth
+sleep 1
+sudo systemctl enable cosmic-greeter.service
+sleep 1
+sudo systemctl enable reflector.service
+sleep 1
+sudo systemctl enable reflector.timer
+sleep 1
+sudo systemctl mask systemd-rfkill.socket
+sleep 1
+sudo systemctl mask systemd-rfkill.service
+sleep 1
+sudo systemctl enable upower
+sleep 1
+sudo systemctl enable grub-btrfsd.service
+sleep 3
+sudo btrfs filesystem mkswapfile --size 8g --uuid clear /.swap/swapfile
+sleep 3
+sudo swapon /.swap/swapfile
+sleep 3
+sudo echo "/.swap/swapfile   none   swap   defaults  0 0" | sudo tee -a /etc/fstab
+sleep 3
+sudo systemctl daemon-reload
+sleep 3
+sudo mount -av
+sleep 3
+free -h
+sleep 1
+sudo grub-mkconfig -o /boot/grub/grub.cfg
+sleep 1
+	echo "################################################################"
+	tput setaf 3;echo "Installing complete"; tput sgr0;
+	echo "################################################################"
+sleep 1
