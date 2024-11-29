a18cc2f02f614326bc2db6a0e14e85d6aa821899
hiruma87 <96128631+hiruma87@users.noreply.github.com>
2024-11-30 07:01:39 +0800

    Update wayfire.sh

--- wayfire.sh
+++ wayfire.sh
@@ -1,7 +1,25 @@
-git clone https://github.com/WayfireWM/wf-install
-cd wf-install
+#!/usr/bin/env bash
 
-./install.sh --prefix /opt/wayfire --stream master
+tput setaf 3
+echo "###############################################################################"
+echo "##################  Installing package Plasma Desktop"
+echo "###############################################################################"
+echo
+tput sgr0
+# installing aur helper yay
+cd "${HOME}"
+mkdir git
+cd git
+echo "CLONING: YAY"
+git clone "https://aur.archlinux.org/yay.git"
+cd yay
+makepkg -si --noconfirm
+
+# installing wayfire from source
+#git clone https://github.com/WayfireWM/wf-install
+#cd wf-install
+
+#./install.sh --prefix /opt/wayfire --stream master
 
 # to update
 # ./update_build.sh . master
